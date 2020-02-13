16cd11d9cbcab20cb4fe7c87ce3b05501a4caea1
90ngrove <54635260+90ngrove@users.noreply.github.com>
2020-02-13 12:13:25 -0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -3,12 +3,9 @@ function buildMyCharts(sample) {
     var samples = data.samples;
     var myresultArray = samples.filter(sampleObj => sampleObj.id == sample);
     var myResults = myresultArray[0];
-
     var ids = myResults.otu_ids;
     var labels = myResults.otu_labels;
     var values = myResults.sample_values;
-
-    // Build a Bubble Chart
     var Layout = {
       title: "Bacteria Cultures",
       margin: { t: 0 },
@@ -51,10 +48,7 @@ function buildMyCharts(sample) {
 }
 
 function init() {
-  // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
     var thesamplelist = data.names;
 
@@ -64,17 +58,12 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-
-    // Use the first sample from the list to build the initial plots
     var initialSample = thesamplelist[0];
     buildMyCharts(initialSample);
   });
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
   buildMyCharts(newSample);
 }
-
-// Initialize the dashboard
 init();
